f1e7e914633faf7f25c72b88077e0303e2f125bd
Mahender-sketch <mhgaira@gmail.com>
2026-03-25 20:24:49 +0530

    Update deploy.js

--- 04-cards-project/deploy.js
+++ 04-cards-project/deploy.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 const distPath = path.resolve('dist');
-const targetPath = path.join(distPath, '04-card-project');
+const targetPath = path.join(distPath, '04-cards-project');
 
 // create subfolder
 if (!fs.existsSync(targetPath)) {
@@ -12,7 +12,7 @@ if (!fs.existsSync(targetPath)) {
 
 // move all files into subfolder
 fs.readdirSync(distPath).forEach(file => {
-  if (file !== '04-card-project') {
+  if (file !== '04-cards-project') {
     fs.renameSync(
       path.join(distPath, file),
       path.join(targetPath, file)
@@ -22,7 +22,7 @@ fs.readdirSync(distPath).forEach(file => {
 
 ghpages.publish(distPath, {
   branch: 'gh-pages',
-  message: 'Deploy 04-card-project',
+  message: 'Deploy 04-cards-project',
 }, (err) => {
   if (err) console.error(err);
   else console.log('Deployed!');
