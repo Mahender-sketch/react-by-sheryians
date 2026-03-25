3fcc866dfb37840e656ef370dc3c66bbe8e15bdf
Mahender-sketch <mhgaira@gmail.com>
2026-03-25 20:24:15 +0530

    Update vite.config.js

--- 04-cards-project/vite.config.js
+++ 04-cards-project/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-by-sheryians/04-card-project/'
+  base: '/react-by-sheryians/04-cards-project/'
 })
